26100562c870b4569ce4e6e1c73f46ea1ca99c83
HoaiGumi <22a1001d0134@students.hou.edu.vn>
2024-08-30 18:19:35 +0700

    DiceApp

---
+++ app/src/main/java/com/example/diceapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.diceapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView diceImage;
+    private Random random;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        diceImage = findViewById(R.id.dice_image);
+
+        random = new Random(System.currentTimeMillis());
+
+        diceImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rotateDice();
+            }
+        });
+    }
+
+    private void rotateDice() {
+        int randomValue = random.nextInt(6) + 1; // Generate random number between 1 and 6
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        diceImage.startAnimation(anim);
+
+        switch (randomValue) {
+            case 1:
+                diceImage.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                diceImage.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                diceImage.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                diceImage.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                diceImage.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                diceImage.setImageResource(R.drawable.dice6);
+                break;
+
+        }
+    }
+}
